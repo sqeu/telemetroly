7b50d864a5ac6807d609600c29e86d0120e0d161
Hugo Castro Aranzabal - EVERIS <hugo_castro5@hotmail.com>
2019-04-16 15:42:23 -0500

    First commit

---
+++ telemetro.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 15:02:48 2019
+
+@author: S80240
+"""
+
+import telemetroly
+from tqdm import tqdm 
+
+query='afp'
+minDate='01-10-2018'
+maxDate='31-10-2018'
+bad_chars='[(){}<>,.@;:"\'?¿!¡/|]' 
+
+
+#q=next(search_query)
+#print(q.bib['title'].replace(' ','_'))
+
+search_query = telemetroly.search_pubs_query(query,minDate,maxDate)
+
+f= open("..//"+query+maxDate+".txt","a+")#,errors = 'ignore'
+for q in tqdm(search_query):
+    try:
+        f.write(q.bib['title']+"|"+q.bib['kicker']+"|"+q.bib['date']+"|"+q.bib['link']+"|"+q.bib['summary']+"|"+q.bib['body']+"\n")
+    except: 
+        f_e= open("..//"+query+maxDate+"_exception.txt","a+")
+        f_e.write(q.bib['title']+"|"+q.bib['kicker']+"|"+q.bib['date']+"|"+q.bib['link']+"\n")
+        f_e.close()
+f.close()
+
+
+
+
+##################################
+q1= next(search_query)
+print(q1)
+
+
+query='Grupo de Lima pide cambio de gobierno en Venezuela "sin uso de la fuerza"'
+minDate='01-02-2019'
+maxDate='28-02-2019'
+bad_chars='[(){}<>,.@;:"\'?¿!¡/|]' 
+
+
+#q=next(search_query)
+#print(q.bib['title'].replace(' ','_'))
+
+search_query = telemetroly.search_pubs_query(query,minDate,maxDate)
+
+f= open("1.txt","a+")#,errors = 'ignore'
+for q in tqdm(search_query):
+    f.write(q.bib['title']+"|"+q.bib['kicker']+"|"+q.bib['date']+"|"+q.bib['link']+"|"+q.bib['summary']+"|"+q.bib['body']+"\n")
+f.close()
---
+++ telemetroly.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 11:03:22 2019
+
+@author: S80240
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from bs4 import BeautifulSoup
+
+import hashlib
+import pprint
+import random
+import requests
+import time
+
+_GOOGLEID = hashlib.md5(str(random.random()).encode('utf-8')).hexdigest()[:16]
+_COOKIES = {'GSP': 'ID={0}:CF=4'.format(_GOOGLEID)}
+_HEADERS = {
+    'accept-language': 'en-US,en',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/41.0.2272.76 Chrome/41.0.2272.76 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml'
+    }
+_HOST = 'https://www.telemetro.com'
+_NEWSSEARCH = '/busqueda/?page={0}&text={1}&minDate={2}&maxDate={3}&contentType=NWS'
+_SESSION = requests.Session()
+_PAGESIZE = 100
+_ENCODING='utf-8'
+
+def _get_page(pagerequest):
+    """Return the data for a page on telemetro.com"""
+    # Note that we include a sleep to avoid overloading the scholar server
+    time.sleep(2+random.uniform(0, 5))
+    resp_url = _SESSION.get(pagerequest, headers=_HEADERS, cookies=_COOKIES,verify=False)
+    if resp_url.status_code == 200:
+        return resp_url.text
+    else:
+        raise Exception('Error: {0} {1}'.format(resp_url.status_code, resp_url.reason))
+        
+def _get_soup(pagerequest):
+    """Return the BeautifulSoup for a page"""
+    html = _get_page(pagerequest)
+    return BeautifulSoup(html, 'lxml')
+
+def _search_in_soup(soup):
+    """Generator that returns Publication objects from the search page"""
+    while True:
+        resultList = soup.find("div", {"class" : "lst-search-results"})
+        for row in resultList.findAll("div", {"class": lambda L: L and L.startswith('mt')}):
+            yield Publication(row)
+        next_= soup.find("a", {"class": lambda L: L and L.startswith('btn btn-next page')})
+        if next_:
+            soup = _get_soup(_HOST+next_['href'])
+        else:
+            break
+
+def search_pubs_query(query,minDate,maxDate,pageNumber=1):
+    """Search by scholar query and return a generator of Publication objects"""
+    soup = _get_soup(_HOST+_NEWSSEARCH.format(pageNumber,query,minDate,maxDate))
+    return _search_in_soup(soup)
+
+def _body_in_soup(article_soup):
+    """Generator that returns Publication objects from the search page"""
+    summary=""
+    body=""
+    resultList = article_soup.find("div", {"class" : lambda L: L and L.startswith('mce-body')})
+    for row in resultList.findAll("p", {"class": 'mce'}):
+        if summary == "":
+            summary = row.text
+        else:  
+            #and not row.find('a').has_attr('target')            
+            if not row.find('a') and not row.has_attr('dir') and not row.has_attr('lang'):
+                body = body +" <br>"+ row.text
+    return (summary,body)
+        
+        
+class Publication(object):
+    """Returns an object for a single publication"""
+    def __init__(self, __data):
+        self.bib = dict()
+        self.bib['title'] = __data.find('a').text
+        if __data.find('span'):
+            self.bib['kicker'] = __data.find('span').text
+        else:
+            self.bib['kicker'] = ""
+        if __data.find('small'):
+            self.bib['date'] = __data.find('small')['title']
+        else:
+            self.bib['date'] = ""
+        self.bib['link'] = __data.find('a',href=True)['href']
+        
+        article_soup = _get_soup(_HOST+__data.find('a',href=True)['href'])
+        body=_body_in_soup(article_soup)
+        
+        self.bib['summary']=body[0]
+        self.bib['body']=body[1]
+                
+    def __str__(self):
+        return pprint.pformat(self.__dict__)
+    
+def get_pdf(url):
+    soup = _get_soup(url,True)
+    head = soup.findAll("head")
+    url = None
+    for row in head:
+        if row.find("meta",{'name':'citation_pdf_url'}) is not None:
+            url = row.find("meta",{'name':'citation_pdf_url'})['content']
+    return url
+                
+    
+
+#from tqdm import tqdm
+#query='bcp'
+#resultado = search_pubs_query('bcp')
+#q=next(resultado)
+#print(q)
+#q.bib['title']
+
+#f= open(query+".txt","x+")
+#for q in tqdm(resultado):
+#    try:
+#        f.write(q.bib['title'].replace(' ','_')+"|"+q.bib['link']+"|"+q.bib['author']+"\n")
+#    except ValueError as e:
+#        print(e.__context__)
+#f.close()
